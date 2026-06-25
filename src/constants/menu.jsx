c86ffa68b3ba1e295aacdb0b0b8bb10f0f3c379e
jeklalap <jerwin.lalap@gbss.com.au>
2026-06-25 11:12:02 +0800

    Added missing menu items

--- src/constants/menu.jsx
+++ src/constants/menu.jsx
@@ -116,7 +116,7 @@ export const MENU_ITEMS = [
             {
                 id: 'COE',
                 name: 'COE',
-                path: '/user/coe',
+                action: 'coe-request',
                 icon: (
                     <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#0d416e">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
@@ -140,6 +140,66 @@ export const MENU_ITEMS = [
                 icon: (
                     <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#0d416e">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
+                    </svg>
+                )
+            },
+        ]
+    },
+    {
+        name: 'Team',
+        submenu: [
+            {
+                id: 'Team-Records',
+                name: 'Records',
+                path: '/team/records',
+                icon: (
+                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#0d416e">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                    </svg>
+                )
+            },
+            {
+                id: 'Team-Leaves',
+                name: 'Leaves',
+                path: '/team/leaves',
+                icon: (
+                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#0d416e">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                    </svg>
+                )
+            },
+            {
+                id: 'Team-Status',
+                name: 'Status',
+                path: '/team/status',
+                icon: (
+                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#0d416e">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                    </svg>
+                )
+            },
+            {
+                id: 'Team-Requests',
+                name: 'Requests',
+                path: '/team/requests',
+                icon: (
+                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#0d416e">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                    </svg>
+                )
+            }
+        ]
+    },
+    {
+        name: 'IT',
+        submenu: [
+            {
+                id: 'Manage-Users',
+                name: 'Manage Users',
+                path: '/it/manage-users',
+                icon: (
+                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#0d416e">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg>
                 )
             },

24099d06c7e31fc135c5e0e0a3343b661e67d8fd
jeklalap <jerwin.lalap@gbss.com.au>
2026-07-03 14:52:27 +0800

    Used existing endpoints for Leaves page

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -6,14 +6,23 @@ export const ROUTES = {
     BULLETIN: {
         STAFF_BULLETINS: '/Email/bulletin/Staff'
     },
+    EMPLOYEE: {
+        GET_DOCUMENTS: (id) => `/Employee/${id}/documents`,
+    },
     FILE: {
         DOWNLOAD: (path) => `/File/download?blobName=${encodeURIComponent(path)}`,
         DELETE: (path) => `/File/${encodeURIComponent(path)}`,
         UPLOAD: '/File/upload',
         GET_BY_FOLDER_PATH: (folderPath) => `/File/${encodeURIComponent(folderPath)}`,
     },
-    EMPLOYEE: {
-        GET_DOCUMENTS: (id) => `/Employee/${id}/documents`,
+    LEAVES: {
+        GET_LEAVE_CREDITS: (personId) => `/Employee/${personId}/leave-credits`,
+        GET_FILED_LEAVES: (personId) => `/Employee/${personId}/leave`,
+        FILE_LEAVE: (personId) => `/Employee/${personId}/leave/add`,
+        FILE_LEAVE_UNPAID: (personId) => `/Employee/${personId}/leave/add/proceedUnPaid`,
+        UPDATE_LEAVE: (personId, leaveId) => `/Employee/${personId}/leave/${leaveId}/update`,
+        UPDATE_LEAVE_UNPAID: (personId, leaveId) => `/Employee/${personId}/leave/${leaveId}/update/proceedUnPaid`,
+        DELETE_LEAVE: (personId, leaveId) => `/Employee/${personId}/leave/${leaveId}/delete`,
     },
     REPORTS: {
         COE_REQUESTS: '/Reports/Pdf/COE',
